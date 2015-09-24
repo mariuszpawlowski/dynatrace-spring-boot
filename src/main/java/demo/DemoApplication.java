893ed9ede00cd534a1f4c9b9b66ced93fb85d942
Mariusz Pawlowski <mariusz.pawlowski@dynatrace.com>
2015-09-24 09:41:52 +0200

    Finished demo app

--- src/main/java/demo/DemoApplication.java
+++ src/main/java/demo/DemoApplication.java
@@ -1,22 +1,9 @@
 package demo;
 
-import java.awt.*;
 import java.util.Arrays;
 import java.util.Collection;
 
-import org.glassfish.jersey.jackson.JacksonFeature;
-import org.glassfish.jersey.server.ResourceConfig;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import javax.cache.annotation.CacheResult;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.ws.rs.ApplicationPath;
@@ -24,26 +11,56 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
+import org.glassfish.jersey.jackson.JacksonFeature;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.ExportMetricWriter;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.boot.actuate.metrics.jmx.JmxMetricWriter;
+import org.springframework.boot.actuate.metrics.writer.MetricWriter;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.http.MediaType;
+import org.springframework.jmx.export.MBeanExporter;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.data.util.BeanItemContainer;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.ui.Table;
+import com.vaadin.ui.UI;
+
+@EnableCaching
 @SpringBootApplication
 public class DemoApplication {
-	
+
 	@Bean
-	CommandLineRunner dummy(ReservationRepository r){
+	CommandLineRunner dummy(ReservationRepository r) {
 		return strings -> {
-			Arrays.asList("jakub,thomas,peter,josh,juergen".split(","))
-			.forEach(n -> r.save(new Reservation(n)));
+			Arrays.asList("jakub,thomas,peter,josh,juergen".split(",")).forEach(n -> r.save(new Reservation(n)));
 
 			r.findAll().forEach(System.out::println);
-
 			r.findByReservationName("jakub").forEach(System.out::println);
 		};
 	}
 
 	@Controller
-	public static class ReservationMvcController{
+	public static class ReservationMvcController {
 
 		@RequestMapping("/page")
-		String page(Model model){
+		String page(Model model) {
 			model.addAttribute("reservations", this.reservationRepository.findAll());
 			return "2";
 		}
@@ -51,23 +68,36 @@ public class DemoApplication {
 		@Autowired
 		ReservationRepository reservationRepository;
 	}
+	
+	@Service
+	public static class ReservationService {
+		
+		@Autowired
+		private ReservationRepository reservationRepository;
+		
+		@CacheResult
+		public Collection<Reservation> getReservations() throws InterruptedException{
+			Thread.sleep(1000*5);
+			return this.reservationRepository.findAll();
+		}
+	}
 
 	@RestController
-	public static class ReservationRestController{
+	public static class ReservationRestController {
 
 		@Autowired
 		private ReservationRepository reservationRepository;
 
 		@RequestMapping("/reservations")
-		Collection<Reservation> reservations(){
+		Collection<Reservation> reservations() {
 			return this.reservationRepository.findAll();
 		}
 	}
 
 	@Named
 	@ApplicationPath("/jersey")
-	public static class JerseyConfig extends ResourceConfig{
-		public JerseyConfig(){
+	public static class JerseyConfig extends ResourceConfig {
+		public JerseyConfig() {
 			register(JacksonFeature.class);
 			register(ReservationEndpoint.class);
 		}
@@ -75,20 +105,53 @@ public class DemoApplication {
 
 	@Named
 	@Path("/reservations")
-	public static class ReservationEndpoint{
+	public static class ReservationEndpoint {
 
 		@Inject
 		private ReservationRepository reservationRepository;
+		
+		@Inject 
+		private ReservationService reservationService; 
 
 		@GET
 		@Produces(MediaType.APPLICATION_JSON_VALUE)
-		public Collection<Reservation> reservations(){
-			return this.reservationRepository.findAll();
+		public Collection<Reservation> reservations() throws InterruptedException {
+			// return this.reservationRepository.findAll();
+			return this.reservationService.getReservations();
 		}
 	}
 
+	@SpringUI(path = "/ui")
+	@Theme("valo")
+	public static class ReservationUI extends UI {
 
-    public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
-    }
+		@Autowired
+		private ReservationRepository reservationRepository;
+
+		@Override
+		protected void init(VaadinRequest request) {
+			Table t = new Table();
+			t.setContainerDataSource(new BeanItemContainer<Reservation>(Reservation.class, this.reservationRepository.findAll()));
+			t.setSizeFull();
+			setContent(t);
+
+		}
+
+	}
+	
+	@Bean
+	HealthIndicator healthIndicator () {
+			return () -> Health.status("Status OK").build();		 
+	}
+	
+	@Bean
+	@ExportMetricWriter
+	@ConditionalOnProperty("spring.jmx.enabled")
+	MetricWriter metricWriter (@Qualifier("mbeanExporter") MBeanExporter exporter){
+		return new JmxMetricWriter(exporter);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
 }
